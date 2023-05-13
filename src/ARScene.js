c53cc5ae49c0fd3ea982b7737ec4e4a4644298d0
DamjanT1988 <Damjan_t@hotmail.com>
2023-05-14 01:57:05 +0200

    2

--- src/ARScene.js
+++ src/ARScene.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { Canvas } from 'react-three-fiber';
-import { ARButton } from '@react-three/drei';
-import { useAR } from '@react-three/xr';
+import { Canvas } from '@react-three/fiber'; // Import Canvas from here
+import { ARButton, XR, useXR } from '@react-three/xr'; // Import ARButton, XR, and useXR
+import { useGLTF } from '@react-three/drei';
 
 const Model = () => {
-  const { gltf } = useLoader(GLTFLoader, '/models/chair/scene.gltf');
-  return <primitive object={gltf.scene} />;
+  const { nodes } = useGLTF('/models/chair/scene.gltf'); // Removed unused 'materials'
+  return <primitive object={nodes.mesh} />;
 };
 
 const ARScene = () => {
-  const { isPresenting } = useAR();
+  const { isPresenting } = useXR();
 
   return (
     <div>
       <Canvas>
-        <ambientLight intensity={0.5} />
-        <directionalLight color="white" intensity={0.6} />
-        <Model />
+        <XR>
+          <ambientLight intensity={0.5} />
+          <directionalLight color="white" intensity={0.6} />
+          <Model />
+        </XR>
       </Canvas>
       {!isPresenting && <ARButton sessionInit={{ requiredFeatures: ['hit-test'], optionalFeatures: ['dom-overlay'], domOverlay: { root: document.body } }} />}
     </div>
